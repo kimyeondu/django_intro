1a640bcfccc2afdf2eb3d17a138ecbb016de9e67
kimyeondu <yjookimcd@gmail.com>
2019-02-14 02:31:37 +0000

    09_Separate URL for multiple apps

---
+++ home/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('static_example/', views.static_example, name='static_example'),
+    path('template_example/', views.template_example, name='template_example'),
+    path('index/', views.index, name='index'),
+    path('user_create/', views.user_create, name='user_create'),
+    path('user_new/', views.user_new, name='user_new'),
+    path('pong/', views.pong, name='pong'),
+    path('ping/', views.ping, name='ping'),
+    path('cube/<int:num>', views.cube, name='cube'),
+    path('hello/<name>', views.hello, name='hello'),
+    path('dinner/', views.dinner, name='dinner'),
+    path('index/', views.index, name='index'),
+    ]
